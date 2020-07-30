3c1b1a0467549c5121db9f8505ae694b7c3f4b12
unknown <Anahita>
2020-07-29 17:15:39 -0700

    Added index.html

---
+++ Assignments/week_5/scripts/main(concatenation and datatype).js
@@ -0,0 +1,24 @@
+var myVariable = 100;
+
+var mySentence = "hundred";
+
+var total = 10 * mySentence;
+
+
+
+var userPurchaseTotal = 55;
+
+var purchaseTax = userPurchaseTotal * .085;
+
+var purchaseTotal = userPurchaseTotal + purchaseTax;
+
+
+var sentenceToWrite = "The user's purchase tax is " + purchaseTax;
+
+var sentenceToWritePurchase = "The user purchased $" + userPurchaseTotal + " worth of stuff!";
+
+var sentenceForTotal ="The user has to pay a total of $" + purchaseTotal;
+
+console.log(sentenceToWritePurchase);
+console.log(sentenceToWrite);
+console.log(sentenceForTotal);
---
+++ Assignments/week_6/javascript/scripts/main.js
@@ -0,0 +1,74 @@
+// The If Statement
+
+//var number1 = 10;
+//var number2 = 15;
+
+// Syntax
+// if (conditional) { group of code to run if conditional is true }
+
+
+// Math Operators in JavaScript.
+// + - / * %
+
+//var number1 = 10;
+//var number2 = 2;
+
+// Addition
+//var sum = number1 + number2;
+
+// Subtraction
+//var difference = number1 - number2;
+//var differenceBackwards = number2 - number1;
+
+// Multip// The If Statement
+
+//var number1 = 10;
+//var number2 = 15;
+
+// Syntax
+// if (conditional) { group of code to run if conditional is true }
+
+
+
+// Conditional Operators
+// == Compares two values, becomes the boolean true if it is the same, and false if they are not the same.
+
+//if (number1 == number2) {
+/*   console.log("This if statement was true so I ran this code!");
+}*/
+
+
+//console.log("Program Finished!");
+//var product = number1 * number2;
+
+// Division
+//var quotient = number1 / number2;
+//var quotientBackwards = number2 / number1;
+
+// Modulo
+//var remainder = number1 % number2;
+
+//console.log("The numbers used are " + number1 + " and " + number2 + "." );
+
+//console.log("Sum: " + sum);
+//console.log("Difference: " + difference + ", Backwards: " + differenceBackwards);
+//console.log("Product: " + product);
+//console.log("Quotient: " + quotient);
+//console.log("Quotient reversed: " + quotientBackwards);
+//console.log("Left overs: " + remainder);
+
+// Functions
+function sayHelloThreeTimes() {
+
+    console.log("hello!");
+    console.log("hello!");
+    console.log("hello!");
+}
+//console.log("hello!")--- wrong choice
+sayHelloThreeTimes()
+sayHelloThreeTimes()
+sayHelloThreeTimes()
+
+
+
+
